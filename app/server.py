766a127399e79270b6e531ae78c1c2f48bb0e0bd
N0Ball <n0ball0325@gmail.com>
2023-08-13 18:31:08 +0800

    CHORE : add CORS middleware

--- app/server.py
+++ app/server.py
@@ -1,10 +1,24 @@
 import json
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.main import generate_response, generate_city_report, generate_taiwan_report, generate_lifetopic_report
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5000",
+    "https://weather-need.n0b.me"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root():
